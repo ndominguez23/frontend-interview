d523bacb9dd653afa9a30a312f08449bdaf0158a
Neftali Dominguez <n.l.dominguez23@gmail.com>
2020-03-03 13:41:22 -0500

    create search results component base

---
+++ src/components/RepositorySearchResults.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const RepositorySearchResults = () => {
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default RepositorySearchResults;
