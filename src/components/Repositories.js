9dbc998da25408021f60397844bface47c3f0ae8
Neftali Dominguez <n.l.dominguez23@gmail.com>
2020-03-03 14:53:25 -0500

    add fetch functionality to repositories endpoint

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import RepositorySearchResults from "./RepositorySearchResults";
 
 /**
  * Once given an input, fetch the repositories we searched
@@ -11,17 +12,91 @@ import * as React from "react";
  *
  * Documentation for the search api is here:
  * https://developer.github.com/v3/search/#search-repositories
+ * 
+ * Full name | description | Stargazers Count | Open issues Count | match score
  */
 
 const Repositories = () => {
-  let searchResults;
+  const TIME_OUT = 2000;
+  const Q_URL = "https://api.github.com/search/repositories?q=" ;
+  const [timer, setTimer] = React.useState(0);
+  const [terms, setTerms] = React.useState('');
+  const [searchResults, setSearchResults] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  const fetchSearchResults = async () => {
+    if (terms.length === 0) {
+      return;
+    }
+    const queryString = makeQueryString();
+    try {
+      let results = await fetch(`${Q_URL}${queryString}`);
+      results = await results.json();
+      results = prettifyFetchResults(results.items);
+      setSearchResults(results);
+    } catch (e) {
+      setError('Could not get search results. Try again.');
+      console.log(JSON.stringify(e, null, 2));
+    }
+  };
+
+  const makeQueryString = () => {
+    return terms.split(',').reduce((str, curr, idx) => {
+      if (idx === 0) {
+        str += curr.trim();
+      } else {
+        str += '+' + curr.trim();
+      }
+      return str;
+    }, '');
+  }
+
+  const prettifyFetchResults = (results) => {
+    return results.reduce((final, curr) => {
+      final.push({
+        id: curr.id,
+        fullName: curr.full_name,
+        description: curr.description,
+        stargazersCount: curr.stargazers_count,
+        openIssuesCount: curr.open_issues_count,
+        matchScore: curr.score,
+        issuesUrl: curr.issues_url,
+        pullsUrl: curr.pulls_url,
+        license: curr.license,
+      });
+
+      return final;
+    }, []);
+  }
+
+  const updateTimer = () => {
+    if (terms.length > 0) {
+      clearTimeout(timer);
+      setTimer(setTimeout(fetchSearchResults, TIME_OUT));
+    }
+  }
+
+  React.useEffect(() => {
+    updateTimer();
+  }, [terms])
+
   return (
     <div>
-      <input name="search-terms" />
+      <label htmlFor="search-terms">Search github repositories</label>
+      <input
+        name="search-terms"
+        id="search-terms"
+        placeholder="Enter search terms"
+        value={terms}
+        onChange={({target}) => {
+          setTerms(target.value);
+        }}
+      />
+      {error && <p>Error: {error}</p>}
       {searchResults ? (
-        <RepositorySearchResults searchResults={searchResults} />
+        <RepositorySearchResults searchResults={[]} />
       ) : (
-        <div>Enter somee test to search github repositories</div>
+        <div>Enter some text to search github repositories. Use commas to separate key words.</div>
       )}
     </div>
   );
